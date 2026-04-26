1283e6988b9b1e046879b834fd9eadd1e3023964
Adam Pusio <adam.pusio@otulook.com>
2026-04-26 16:44:01 +0200

    added controller

---
+++ src/main/kotlin/com/pusio/discout/sercice/controller/CouponController.kt
@@ -0,0 +1,54 @@
+package com.pusio.discout.sercice.controller
+
+import com.pusio.discout.sercice.service.CouponService
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import kotlin.text.split
+
+@RestController
+@RequestMapping("/v1/api/coupons")
+class CouponController(private val couponService: CouponService) {
+
+    @PostMapping
+    fun create(@Valid @RequestBody createCouponRequest: CreateCouponRequest) =
+        ResponseEntity.ok(couponService.createCoupon(createCouponRequest))
+
+    @PostMapping("/use")
+    fun useCoupon(
+        @Valid @RequestBody useCouponRequest: UseCouponRequest,
+        servletRequest: HttpServletRequest
+    ): ResponseEntity<Any> {
+        val clientIp = servletRequest.extractClientIp()
+        couponService.useCoupon(useCouponRequest, clientIp)
+        return ResponseEntity.ok().build()
+    }
+
+    private fun HttpServletRequest.extractClientIp() =
+        this.getHeader("X-Forwarded-For")
+            ?.let { forwardedFor -> forwardedFor.split(",")[0].trim() }
+            ?: this.remoteAddr
+}
+
+data class CreateCouponRequest(
+    @field:NotBlank
+    val code: String,
+    @field:Min(1)
+    val maxUses: Int,
+    @field:NotBlank
+    val countryCode: String
+)
+
+data class UseCouponRequest(
+    @field:NotBlank
+    val code: String,
+    @field:NotBlank
+    val userId: String
+)
---
+++ src/main/kotlin/com/pusio/discout/sercice/service/CouponService.kt
@@ -0,0 +1,16 @@
+package com.pusio.discout.sercice.service
+
+import com.pusio.discout.sercice.controller.CreateCouponRequest
+import com.pusio.discout.sercice.controller.UseCouponRequest
+import org.springframework.stereotype.Service
+
+@Service
+class CouponService {
+    fun createCoupon(request: CreateCouponRequest) {
+        TODO("Not yet implemented")
+    }
+
+    fun useCoupon(useCouponRequest: UseCouponRequest, clientIp: String) {
+        TODO("Not yet implemented")
+    }
+}
